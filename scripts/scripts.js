8aefb7b79f6ca75fd5da23de98a632e6d5b4c9e6
Ryan <ryanjonesirl@gmail.com>
2017-02-14 10:49:09 -0800

    add a differnt button class when the button is pressed, switching from dark to light

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -26,10 +26,12 @@ $(document).ready(function() {
     if(x == 0) {
       $("body").removeClass().addClass("bg-dark");
       $("#bg-switch").text("Light Background");
+      $("button").removeClass("btn-dark").addClass("btn-light");
       x = 1;
     } else {
-      $("body").removeClass().addClass('bg-light');
+      $("body").removeClass().addClass("bg-light");
       $("#bg-switch").text("Dark Background");
+      $("button").removeClass("btn-light").addClass("btn-dark");
       x = 0;
     }
   });
